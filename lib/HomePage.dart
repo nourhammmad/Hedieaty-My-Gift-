fb89c2102d12b17e38b76a6e3656e4cb8cb89dc2
nourhammmad <nournourt614@gmail.com>
2024-10-16 20:18:08 +0300

    Initial Configurations

---
+++ lib/HomePage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(title:const Text("Hedieaty",style: TextStyle(fontSize: 50,fontFamily: "Lobster",fontWeight:FontWeight.bold),),centerTitle: true,toolbarHeight: 70,),
+    );
+  }
+}
